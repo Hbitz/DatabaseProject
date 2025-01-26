0c22259a3c8a9a0c1714d11a51bb28d784e2bdcb
Hbitz <53080862+Hbitz@users.noreply.github.com>
2025-01-26 09:21:13 +0100

    Add project files.

---
+++ DatabaseProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DatabaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
